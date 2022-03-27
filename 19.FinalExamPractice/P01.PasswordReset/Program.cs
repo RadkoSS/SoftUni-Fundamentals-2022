5963f921c76f74bb34b9623d70242623e8b7adf5
Radoslav Gervaziev <radi7275@gmail.com>
2022-03-27 21:35:49 +0300

    Another day, another practice

---
+++ 19.FinalExamPractice/P01.PasswordReset/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
