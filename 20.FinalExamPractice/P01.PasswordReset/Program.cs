d82aeb15555ec847cd6cef403a5ee713bc1bf9c5
Radoslav Gervaziev <radi7275@gmail.com>
2022-03-28 18:32:22 +0300

    P01. Successfully solved

--- 20.FinalExamPractice/P01.PasswordReset/Program.cs
+++ 20.FinalExamPractice/P01.PasswordReset/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace P01.PasswordReset
 {
@@ -6,7 +7,67 @@ namespace P01.PasswordReset
     {
         static void Main()
         {
-            
+            string wrongPassword = Console.ReadLine();
+
+            string action;
+            while ((action = Console.ReadLine()) != "Done")
+            {
+                string[] commands = action.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (commands[0] == "TakeOdd")
+                {
+                    wrongPassword = TakeOddChars(wrongPassword);
+                    Console.WriteLine(wrongPassword);
+                }
+
+                else if (commands[0] == "Cut")
+                {
+                    int index = int.Parse(commands[1]);
+                    int lenght = int.Parse(commands[2]);
+
+                    wrongPassword = CutString(wrongPassword, index, lenght);
+                    Console.WriteLine(wrongPassword);
+                }
+
+                else if (commands[0] == "Substitute")
+                {
+                    string oldString = commands[1];
+                    string newString = commands[2];
+
+                    if (!wrongPassword.Contains(oldString))
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                        continue;
+                    }
+                    wrongPassword = SubstituteStrings(wrongPassword, oldString, newString);
+
+                    Console.WriteLine(wrongPassword);
+                }
+            }
+
+            Console.WriteLine($"Your password is: {wrongPassword}");
+        }
+
+        static string TakeOddChars(string wrongPassword)
+        {
+            StringBuilder rawPassword = new StringBuilder();
+
+            for (int symbolNum = 1; symbolNum < wrongPassword.Length; symbolNum += 2)
+            {
+                rawPassword.Append(wrongPassword[symbolNum]);
+            }
+
+            return rawPassword.ToString();
+        }
+
+        static string CutString(string wrongPassword, int index, int lenght)
+        {
+            return wrongPassword.Remove(index, lenght);
+        }
+
+        static string SubstituteStrings(string wrongPassword, string oldString, string newString)
+        {
+            return wrongPassword.Replace(oldString, newString);
         }
     }
 }
